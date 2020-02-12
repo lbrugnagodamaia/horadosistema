f53b8099b4ab6c72d15e715bc4436b50cd73069f
lbrugnagodamaia <l.brugnago@hotmail.com>
2020-02-12 20:55:56 -0300

    Initial commit

---
+++ src/horadosistema/HoraDoSitema.java
@@ -0,0 +1,14 @@
+package horadosistema;
+
+import java.util.Date;
+
+public class HoraDoSitema {
+
+	public static void main(String[] args) {
+		
+		Date relogio = new Date();
+		System.out.println("A hora do sistema é: ");
+		System.out.println(relogio.toString());
+	}
+
+}
